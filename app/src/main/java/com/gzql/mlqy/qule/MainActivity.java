48afd3fc71ed8728320c93a24bfeda1a447d90e7
lonelywo <850411893@qq.com>
2017-06-13 17:50:02 +0800

    新项目

---
+++ app/src/main/java/com/gzql/mlqy/qule/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gzql.mlqy.qule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
